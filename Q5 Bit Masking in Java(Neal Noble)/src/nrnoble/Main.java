d2e05411c7e2e1153af57a2fb1e6ffa676bc1820
Neal Noble <nrnoble@hotmail.com>
2016-01-25 02:21:42 -0800

    updating java files

--- Q5 Bit Masking in Java(Neal Noble)/src/nrnoble/Main.java
+++ Q5 Bit Masking in Java(Neal Noble)/src/nrnoble/Main.java
@@ -10,7 +10,6 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		
 		BooleanArray bitArray = new BooleanArray();
 		System.out.println("Starting Bit Tests");
